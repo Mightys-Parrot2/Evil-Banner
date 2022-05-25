0254bb473c430de1b49d1548636cfbbca222029f
Mightys-Parrot2 <105868831+Mightys-Parrot2@users.noreply.github.com>
2022-05-25 07:18:16 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+figlet -f slant "Installing"|lolcat
+sleep 2
+pkg install ruby ; gem install lolcat
+pkg install wget
+gem install lolcat
+pkg install figlet
+pkg install cowsay
+figlet -f slant "Done"|lolcat
+echo -e
+echo -e "\e[1;31m  run tool bash ebanner.sh
